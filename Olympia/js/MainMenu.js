4d0a740a47de9ed4cbf6ecea257ecff6946b1555
UnknownAeon <kpfeifle@gmu.edu>
2018-04-24 16:04:39 -0400

    init commit for Olympia

---
+++ Olympia/js/MainMenu.js
@@ -0,0 +1,93 @@
+"use strict";
+
+GameStates.makeMainMenu = function( game, shared ) {
+
+	var music = null;
+	var playButton = null;
+
+  function startGame(pointer) {
+		let buttonClick = game.add.audio('menuSelect');
+		buttonClick.volume = .3;
+		buttonClick.play();
+  	//	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
+    music.stop();
+    //	And start the actual game
+    game.state.start('Game');
+    }
+
+  return {
+  	create: function () {
+    	//	We've already preloaded our assets, so let's kick right into the Main Menu itself.
+      music = game.add.audio('titleMusic');
+			music.loop = true;
+			music.volume = .3;
+      music.play();
+
+			let background = game.add.sprite(0, 0, 'titleBackground');
+			// Sets up the button in the center of the screen.
+			let playButtonFrame = game.add.sprite(game.world.centerX - 132, 350, 'buttonFrame');
+      let playButton = game.add.button(game.world.centerX - 128, 353, 'buttons', startGame, null, 0, 2, 4);
+
+			let title = game.add.sprite(0, 0, 'title');
+
+			// Let's add some penguins to the title screen to give it some personality!
+			// The animated walking pengiun on the main menu.
+			this.p1 = game.add.sprite(210, 420, 'penguins');
+			this.p1.animations.add('walkLeft', ['p1_left1', 'p1_left2', 'p1_left3', 'p1_left2'], 4, true);
+			this.p1.animations.add('walkRight', ['p1_right1', 'p1_right2', 'p1_right3', 'p1_right2'], 4, true);
+			this.p1.animations.play('walkRight');
+			this.p1MoveRight = game.add.tween(this.p1).to({x: '+150'}, 2500, Phaser.Easing.Linear.None, true);
+			this.p1MoveLeft = game.add.tween(this.p1).to({x: '-150'}, 2500, Phaser.Easing.Linear.None, false);
+			this.p1LastMove = 'right';
+
+			// The animated sliding penguin on the main menu.
+			this.p2 = game.add.sprite(760, 30, 'penguins');
+			this.p2.animations.add('slideLeft', ['p2_slide_left'], 1, false);
+			this.p2.animations.add('slideRight', ['p2_slide_right'], 1, false);
+			this.p2.animations.play('slideLeft');
+			this.p2MoveRight = game.add.tween(this.p2).to({x: '+525'}, 4000, Phaser.Easing.Linear.None, false);
+			this.p2MoveLeft = game.add.tween(this.p2).to({x: '-525'}, 4000, Phaser.Easing.Linear.None, true);
+			this.p2LastMove = 'left';
+			// All the penguins that don't move.
+			let p3 = game.add.sprite(590, 440, 'penguins');
+			p3.animations.add('stand', ['p3_right2'], 1, false);
+			p3.animations.play('stand');
+			let p4 = game.add.sprite(625, 440, 'penguins');
+			p4.animations.add('stand', ['p4_left2'], 1, false);
+			p4.animations.play('stand');
+
+			let p5 = game.add.sprite(110, 520, 'penguins');
+			p5.animations.add('stand', ['p3_idle'], 1, false);
+			p5.animations.play('stand');
+			let p6 = game.add.sprite(735, 370, 'penguins');
+			p6.animations.add('stand', ['p2_right2'], 1, false);
+			p6.animations.play('stand');
+		},
+
+    update: function () {
+			// Tween updates for penguin 1
+			if (!this.p1MoveRight.isRunning && this.p1LastMove == 'right') {
+				this.p1.animations.play('walkLeft');
+				this.p1MoveLeft.start();
+				this.p1LastMove = 'left';
+			}
+			else if (!this.p1MoveLeft.isRunning && this.p1LastMove == 'left') {
+				this.p1.animations.play('walkRight');
+				this.p1MoveRight.start();
+				this.p1LastMove = 'right';
+			}
+
+			// Tween updates for penguin 2
+			if (!this.p2MoveRight.isRunning && this.p2LastMove == 'right') {
+				this.p2.animations.play('slideLeft');
+				this.p2MoveLeft.start();
+				this.p2LastMove = 'left';
+			}
+			else if (!this.p2MoveLeft.isRunning && this.p2LastMove == 'left') {
+				this.p2.animations.play('slideRight');
+				this.p2MoveRight.start();
+				this.p2LastMove = 'right';
+			}
+		}
+  };
+};
---
+++ Olympia/js/Preloader.js
@@ -0,0 +1,86 @@
+"use strict";
+
+GameStates.makePreloader = function( game ) {
+	var background = null;
+	var preloadBar = null;
+	var ready = false;
+
+  return {
+    preload: function () {
+    	//	These are the assets we loaded in Boot.js
+      //	A nice sparkly background and a loading progress bar
+      let background = game.add.sprite(0, 0, 'titleBackground');
+      let preloadBarFrame = game.add.sprite(0, 0, 'loadingBarFrame');
+			preloadBarFrame.width = 800;
+			preloadBarFrame.height = 600;
+			preloadBar = game.add.sprite(0, 0, 'loadingBar');
+			preloadBar.width = 800;
+			preloadBar.height = 600;
+
+      //	This sets the preloadBar sprite as a loader sprite.
+      //	What that does is automatically crop the sprite from 0 to full-width
+      //	as the files below are loaded in.
+      game.load.setPreloadSprite(preloadBar);
+
+      //	Here we load the rest of the assets our game needs.
+
+			// All of the mainmenu stuffs.
+      game.load.audio('titleMusic', 'assets/audio/music/ice_cream_sandwich.mp3');
+			game.load.image('buttonFrame', 'assets/images/button_frame.png');
+			game.load.image('title', 'assets/images/title.png');
+
+			// All the ingame stuffs.
+			game.load.image('background', 'assets/images/background.png');
+			game.load.image('water', 'assets/sprites/water.png');
+			game.load.image('spawnTiles', 'assets/sprites/spawnTiles.png');
+			game.load.image('flipDirection', 'assets/sprites/flipDirection.png');
+			game.load.image('bear_death', 'assets/sprites/bear_death.png');
+
+			// All the levels.
+			game.load.tilemap('level1', 'assets/levels/level1.json', null, Phaser.Tilemap.TILED_JSON);
+			game.load.tilemap('level2', 'assets/levels/level2.json', null, Phaser.Tilemap.TILED_JSON);
+			game.load.tilemap('level3', 'assets/levels/level3.json', null, Phaser.Tilemap.TILED_JSON);
+			game.load.tilemap('level4', 'assets/levels/level4.json', null, Phaser.Tilemap.TILED_JSON);
+			game.load.tilemap('level5', 'assets/levels/level5.json', null, Phaser.Tilemap.TILED_JSON);
+
+			// All the sprite stuffs.
+			game.load.spritesheet('buttons', 'assets/images/buttons.png', 257, 48);
+			game.load.spritesheet('hourglass', 'assets/sprites/hourglass.png', 32, 32);
+			game.load.spritesheet('iceBlock', 'assets/sprites/ice_block.png', 32, 32);
+			game.load.spritesheet('blueFish', 'assets/sprites/fish_blue.png', 14, 21);
+			game.load.spritesheet('greenFish', 'assets/sprites/fish_green.png', 21, 9);
+			game.load.atlas('penguins', 'assets/sprites/penguins.png', 'assets/sprites/penguins.json');
+			game.load.atlas('bears', 'assets/sprites/polarbears.png', 'assets/sprites/bears.json');
+
+			// All the level finished stuffs.
+			game.load.image('levelComplete', 'assets/images/levelComplete.png');
+			game.load.image('levelFailed', 'assets/images/levelFailed.png');
+			game.load.image('victory', 'assets/images/victory.png');
+			game.load.spritesheet('noStar', 'assets/sprites/noStar.png', 32, 32);
+			game.load.spritesheet('star', 'assets/sprites/star.png', 32, 32);
+
+			// All the sound effects stuffs.
+			game.load.audio('jump', 'assets/audio/sounds/jump.wav');
+			game.load.audio('bearKilled', 'assets/audio/sounds/bear_killed.wav');
+			game.load.audio('blockStep', 'assets/audio/sounds/block_step.wav');
+			game.load.audio('menuSelect', 'assets/audio/sounds/menu_select.wav');
+			game.load.audio('pickup', 'assets/audio/sounds/pickup.wav');
+			game.load.audio('playerKilled', 'assets/audio/sounds/player_killed.wav');
+			game.load.audio('playerWater', 'assets/audio/sounds/player_water.wav');
+			game.load.audio('powerup', 'assets/audio/sounds/powerup.wav');
+    },
+
+    create: function () {
+	    //	Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
+	    preloadBar.cropEnabled = false;
+    },
+
+    update: function () {
+			// Waits for music to be decoded entirely before the game can begin.
+      if (game.cache.isSoundDecoded('titleMusic') && ready == false) {
+        ready = true;
+        game.state.start('MainMenu');
+      }
+    }
+  };
+};
